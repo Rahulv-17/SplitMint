988f471e9b7a87715de01847bff3fdf1c89e979e
Vaddi Rahul <vaddirahul3003@gmail.com>
2026-08-22 21:37:22 +0530

    Changed brand logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = ({ className = "h-8" }) => {
   return (
     <div className={`relative flex items-center justify-center ${className}`}>
       <img 
-        src="/logo.png" 
+        src="/logo.jpg" 
         alt="SplitMint Logo" 
         className="h-full w-auto object-contain drop-shadow-[0_0_15px_rgba(0,245,160,0.2)] hover:drop-shadow-[0_0_25px_rgba(0,245,160,0.4)] transition-all duration-300"
         style={{ mixBlendMode: 'lighten' }}
